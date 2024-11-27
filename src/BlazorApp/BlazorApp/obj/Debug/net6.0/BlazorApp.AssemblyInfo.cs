df9839083d188948a21bb71928b3f11257134b70
v-shub <victoriya.shubina.mex@gmail.com>
2024-11-27 13:06:34 +0300

    fix textarea + add input clearing

--- src/BlazorApp/BlazorApp/obj/Debug/net6.0/BlazorApp.AssemblyInfo.cs
+++ src/BlazorApp/BlazorApp/obj/Debug/net6.0/BlazorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52fe4f121ba5de9d6aa740a2c1da7b97ef3591ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89e9bbe867949dcbe64792cf99c7a197a2dcc970")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
