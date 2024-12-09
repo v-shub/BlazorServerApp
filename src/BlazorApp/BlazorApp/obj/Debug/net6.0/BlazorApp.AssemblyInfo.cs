19fad8886d50ec4251b4f0215f71ca697755e4dd
Levashov_Mihail <mikhail.levashov.05@email.com>
2024-12-09 10:04:04 +0300

    fix conn string

--- src/BlazorApp/BlazorApp/obj/Debug/net6.0/BlazorApp.AssemblyInfo.cs
+++ src/BlazorApp/BlazorApp/obj/Debug/net6.0/BlazorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89e9bbe867949dcbe64792cf99c7a197a2dcc970")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
